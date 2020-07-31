2972197d498a90ebf1937906db3351e57884d8ce
Tony3641 <tony3641@126.com>
2020-08-01 02:18:19 +0800

    Fix DFS. TODO: BFS path

--- Graph.cpp
+++ Graph.cpp
@@ -4,89 +4,101 @@
 
 #include "Graph.h"
 #include <iostream>
-#include <bits/stdc++.h>
+#include <list>
+
+
 
 Graph::Graph(int v) {
     this->numV=v;
     adj=new list<int>[v];
-    visited_init();
 }
 
-void Graph::visited_init() {
-    visited = new bool[numV];
-    for(int i=0; i<numV; i++){
-        visited[numV]=false;
-    }
-}
 
 void Graph::add_edge(int v1, int v2) {
     adj[v1].push_back(v2);
-}
-
-bool Graph::DFS_Internal(int v, int target, bool visited[], list<int> *path) {
-    bool state = false;
-    visited[v]= true;
-    if(target==v) {
-        state = true;
-        path->push_back(v);
-    }
-    else {
-        list<int>::iterator i;
-        for (i = adj[v].begin(); i != adj[v].end(); ++i) {
-            if (!visited[*i]){
-                state = DFS_Internal(*i, target, visited, path);
-                path->push_back(v);
-            }
-        }
-    }
-    return state;
-}
-
-bool Graph::DFS(int start, int target, list<int> *path) {
-    visited_init();
-    bool found = DFS_Internal(start, target, visited, path);
-    path->reverse();
-    return found;
-}
-
-bool Graph::BFS(int start, int target){
-    visited_init();
-    bool state = false;
-
-    list<int> q;
-    visited[start]=true;
-
-    q.push_back(start);
-    list<int>::iterator i;
-    while(!q.empty()) {
-        if(q.back()==target) {
-            state = true;
-            break;
-        }
-        q.pop_front();
-
-        for (i = adj[start].begin(); i != adj[start].end(); ++i) {
-            if(*i==target){
-                state = true;
-                visited[*i] = true;
-                break;
-            }
-            if (!visited[*i]) {
-                visited[*i] = true;
-                q.push_back(*i);
-            }
-        }
-    }
-    return state;
 }
 
 void Graph::print_path(list<int> *path) {
     list<int>::iterator i;
     for(i = path->begin(); i!= path->end(); ++i){
         if(*i!=path->back())
-            cout<<*i<<"->";
+            cout<<*i<<" -> ";
         else {
             cout << *i << endl;
         }
     }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//////////////////////////////-----DFS-----//////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+bool Graph::DFS_Internal(int v, int target, bool *visited, list<int> *path) {
+    bool found = false;
+
+    //cout << "Visiting " << v << endl;
+    visited[v] = true;
+
+    if(v==target){
+        path->push_back(v);
+        return true;
+    }
+    else{
+        for(auto i = adj[v].begin(); i!=adj[v].end(); i++){
+            if(!visited[*i]){
+                //cout<<v<<"->"<<*i<<endl;
+                path->push_back(v);
+                found = DFS_Internal(*i, target, visited, path);
+            }
+            if(found){
+                break;
+            }
+            else{
+                path->pop_back();
+            }
+        }
+        return found;
+    }
+
+}
+
+bool Graph::DFS(int start, int target, list<int> *path) {
+    bool *visited = new bool[numV];
+    for(int i=0; i<numV; i++){
+        visited[i]=false;
+    }
+    DFS_Internal(start, target, visited, path);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//////////////////////////////-----BFS-----//////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+bool Graph::BFS(int start, int target, list<int> *path){
+    bool *visited = new bool[numV];
+    for(int i=0; i<numV; i++){
+        visited[i]=false;
+    }
+
+    bool state = false;
+
+    list<int> q;
+    visited[start]=true;
+    q.push_back(start);
+    while(!q.empty()) {
+        if(q.front()==target) {
+            return true;
+        }
+        start = q.front();
+        q.pop_front();
+
+        cout<<"Checking "<<start<<"adj list"<<endl;
+
+        for (auto i = adj[start].begin(); i != adj[start].end(); ++i) {
+            if (!visited[*i]) {
+                cout<<"Visit and enqueue"<<*i<<endl;
+                visited[*i] = true;
+                q.push_back(*i);
+            }
+        }
+    }
+    return state;
 }
--- Graph.h
+++ Graph.h
@@ -2,7 +2,7 @@
 // Created by tony3 on 5/25/2020.
 //
 #include <iostream>
-#include <bits/stdc++.h>
+#include <list>
 
 #ifndef SEARCH_GRAPH_H
 #define SEARCH_GRAPH_H
@@ -12,14 +12,12 @@ using namespace std;
 class Graph {
     int numV;
     list<int> *adj;
-    bool *visited;
-    bool DFS_Internal(int v, int target, bool visited[], list<int> *path);
+    bool DFS_Internal(int v, int target, bool visited[],list<int> *path);
 public:
     Graph(int v);
-    void visited_init();
     void add_edge(int v1, int v2);
-    bool DFS(int start, int target, list<int> *path);
-    bool BFS(int start, int target);
+    bool DFS(int start, int target,list<int> *path);
+    bool BFS(int start, int target,list<int> *path);
     void print_path(list<int> *path);
 };
 
